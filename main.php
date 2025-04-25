2f0017f71539e34de678cd848a2b6b9f37d77eb4
bobombb <biancaaemnas@gmail.com>
2025-04-24 23:43:57 -0700

    Add files via upload

--- main.php
+++ main.php
@@ -38,58 +38,112 @@ else {
 }
 
 ?>
-			<div id="header">
-				<div class="container">
+	<div id="header">
+		<div class="container">
 						
-					<!-- Logo -->
-						<h1><a href="index.html"><img src="images/logo.png" width="400"></a></h1>
+			<!-- Logo -->
+			<h1><a href="index.html"><img src="images/logo.png" width="400"></a></h1>
 					
-					<!-- Nav -->
-						<nav id="nav">
-							<ul>
-								<li><a href="index.html">Home</a></li>
-								<li><a href="left-sidebar.html">About Us</a></li>
-								<li>
-									<a href="Services.html">Services</a>
-									<ul>
-										<li><a href="ServicesDetails.html#bartending">Bartending</a></li>
-										<li><a href="ServicesDetails.html#catering">Catering</a></li>
-										<li><a href="ServicesDetails.html#entertainment">Entertainment</a></li>
-										<li><a href="ServicesDetails.html#photography">Photography</a></li>
-									</ul>
-								</li>
-<!--								<li><a href="right-sidebar.html">Iniquiries</a></li>-->
-								<li><a href="contact-us.html">Contact Us</a></li>
-							</ul>
-						</nav>
+			<!-- Nav -->
+			<nav id="nav">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="left-sidebar.html">About Us</a></li>
+					<li>
+						<a href="Services.html">Services</a>
+						<ul>
+							<li><a href="ServicesDetails.html#bartending">Bartending</a></li>
+							<li><a href="ServicesDetails.html#catering">Catering</a></li>
+							<li><a href="ServicesDetails.html#entertainment">Entertainment</a></li>
+							<li><a href="ServicesDetails.html#photography">Photography</a></li>
+						</ul>
+					</li>
+					<li><a href="contact-us.html">Contact Us</a></li>
+				</ul>
+			</nav>
 
+		</div>
+	</div>
+
+	<div class="wrapper style1">
+		<div class="container">
+
+			<br>
+			<h2>Welcome back, <?php print $username; ?>!</h2>
+			<br>
+
+			<!--when button clicked, go to code for incrementing count-->
+			<form action="code.php" method="post">
+    			<button type="submit" class="button alt" name="click">CLICK ME!</button>
+				<br><br>
+    			<div id="content" class="8u skel-cell-important">
+   		 			<!--display user's current count-->
+    				<p> Count: <?php print $count; ?></p>
+				</div>
+			</form>
+			<br>
+
+			<form action="logout.php" method="post">
+    			<button type="submit" class="button small" name="logout">logout</button>
+				<br><br>
+			</form>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<div id="footer">
+		<div class="container">
+
+		<!-- Lists -->
+			<div class="row">
+				<div class="8u">
+					<section>
+						<header class="major">
+							<h2>Urban Oasis</h2>
+							<span class="byline">Event Venue</span>
+						</header>
+						<div class="row">
+							<section class="6u">
+								<ul class="default">
+									<li><a href="Services.html#inquiryButton">Submit An Inquiry</a></li>
+									<li><a href="Services.html">Check Out Our Services</a></li>
+									<li><a href="ServicesDetails.html">View Available Service Packages</a></li>
+								</ul>
+							</section>
+						</div>
+					</section>
+				</div>
+				<div class="4u">
+					<section>
+						<header class="major">
+							<h2>Get In Touch</h2>
+							<span class="byline">Book With Us Today</span>
+						</header>
+						<ul class="contact">
+							<li>
+								<span class="address">Address</span>
+								<span>1811 Nordhoff St <br />Northridge, CA 91330</span>
+							</li>
+							<li>
+								<span class="mail">Mail</span>
+								<span><a href="contact-us.html">Contact US</a></span>
+							</li>
+							<li>
+								<span class="phone">Phone</span>
+								<span>(123) 456-7890</span>
+							</li>
+						</ul>	
+					</section>
 				</div>
 			</div>
 
-<div class="wrapper style1">
-	<div class="container">
+					<!-- Copyright
+						<div class="copyright">
+							Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
+						</div> -->
 
-		<br>
-		<h2>Welcome back, <?php print $username; ?>!</h2>
-		<br>
-
-		<!--when button clicked, go to code for incrementing count-->
-		<form action="code.php" method="post">
-    		<button type="submit" class="button alt" name="click">CLICK ME!</button>
-			<br><br>
-    		<div id="content" class="8u skel-cell-important">
-    			<!--display user's current count-->
-    			<p> Count: <?php print $count; ?></p>
-			</div>
-		</form>
-		<br>
-
-		<form action="logout.php" method="post">
-    		<button type="submit" class="button small" name="logout">logout</button>
-			<br><br>
-		</form>
+		</div>
 	</div>
-</div>
 
 </body>
 </html>
