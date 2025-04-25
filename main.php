032bbddde830387c4c2c3e4ff3d9ceb2f67e30f4
bobombb <biancaaemnas@gmail.com>
2025-04-24 23:30:10 -0700

    Add files via upload

--- main.php
+++ main.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8" />
     <title>Main</title>
+    <link rel="stylesheet" href="css/skel.css" />
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <?php 
@@ -15,8 +17,7 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 //open sql connection
-//$mysqli = new mysqli ("localhost", "root", "root", "userdb");
-require 'db.php';
+$mysqli = new mysqli ("localhost", "root", "root", "userdb");
 								
 if ($mysqli->connect_error) {
     print "Error is: " . $mysqli->connect_error;
@@ -37,21 +38,58 @@ else {
 }
 
 ?>
+			<div id="header">
+				<div class="container">
+						
+					<!-- Logo -->
+						<h1><a href="index.html"><img src="images/logo.png" width="400"></a></h1>
+					
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="left-sidebar.html">About Us</a></li>
+								<li>
+									<a href="Services.html">Services</a>
+									<ul>
+										<li><a href="ServicesDetails.html#bartending">Bartending</a></li>
+										<li><a href="ServicesDetails.html#catering">Catering</a></li>
+										<li><a href="ServicesDetails.html#entertainment">Entertainment</a></li>
+										<li><a href="ServicesDetails.html#photography">Photography</a></li>
+									</ul>
+								</li>
+<!--								<li><a href="right-sidebar.html">Iniquiries</a></li>-->
+								<li><a href="contact-us.html">Contact Us</a></li>
+							</ul>
+						</nav>
 
-<p>Welcome back, <?php print $username; ?>!</p>
-<br>
+				</div>
+			</div>
 
-<!--when button clicked, go to code for incrementing count-->
-<form action="code.php" method="post">
-    <button type="submit" class="btn btn-default" name="click">CLICK ME!</button>
-    <!--display user's current count-->
-    <p> Count: <?php print $count; ?></p>
-</form>
-<br>
+<div class="wrapper style1">
+	<div class="container">
 
-<form action="logout.php" method="post">
-    <button type="submit" class="btn btn-default" name="logout">logout</button>
-</form>
+		<br>
+		<h2>Welcome back, <?php print $username; ?>!</h2>
+		<br>
+
+		<!--when button clicked, go to code for incrementing count-->
+		<form action="code.php" method="post">
+    		<button type="submit" class="button alt" name="click">CLICK ME!</button>
+			<br><br>
+    		<div id="content" class="8u skel-cell-important">
+    			<!--display user's current count-->
+    			<p> Count: <?php print $count; ?></p>
+			</div>
+		</form>
+		<br>
+
+		<form action="logout.php" method="post">
+    		<button type="submit" class="button small" name="logout">logout</button>
+			<br><br>
+		</form>
+	</div>
+</div>
 
 </body>
 </html>
