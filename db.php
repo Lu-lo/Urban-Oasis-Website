49b1ed83ccf35cdbe972e1b3e44325ee2092bf50
bobombb <biancaaemnas@gmail.com>
2025-04-26 23:45:10 -0700

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,3 @@
 <?php
-$user = "dbuser";
-$password = "password";
-$database = "userdb";
-$table = "user";
-
-try {
-  $mysqli = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-
-} 
-catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
+$mysqli = new mysqli ("localhost", "dbuser", "password", "userdb");
 ?>
